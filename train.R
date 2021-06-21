ada164f86dcdfb3ace5440539c855bc7b1f1800d
Willy Tadema <friesewoudloper@gmail.com>
2021-06-21 13:10:49 +0200

    Update train.R

--- train.R
+++ train.R
@@ -1,25 +1,20 @@
 # !source mlflow::mlflow_run(entry_point = .file)
 
 library(mlflow)
-library(glmnet)
-
-wine_file <- pins::pin("https://raw.githubusercontent.com/rstudio/mlflow-example/master/wine-quality.csv")
-
-train <- read.csv(wine_file)
-
-train_x <- as.matrix(train[, !(names(train) == "quality")])
-train_y <- train[, "quality"]
+library(rpart)
 
 with(mlflow_start_run(), {
-  alpha <- mlflow_log_param("alpha", mlflow_param("alpha", 0.7))
-  lambda <- mlflow_log_param("lambda", mlflow_param("lambda", 0.4))
-  
-  model <- glmnet(train_x, train_y, alpha = alpha, lambda = lambda, family = "gaussian")
-  predicted <- glmnet::predict.glmnet(model, train_x)
-  
-  rmse <- sqrt(mean((predicted - train_y)^2))
-  mae <- mean(abs(predicted - train_y))
-  
-  mlflow_log_metric("rmse", rmse)
-  mlflow_log_metric("mae", mae)
+  cp <- mlflow_param("cp", default = 0.05)
+  maxdepth <- mlflow_param("maxdepth", default = 3)
+  tree <- rpart(
+    Species ~ .,
+    data = iris,
+    method = "class",
+    control = rpart.control(cp = cp, maxdepth = maxdepth)
+  )
+  predictor <- carrier::crate(
+    function(x) {rpart:::predict.rpart(model, x, type = "class")},
+    model = tree
+  )
+  mlflow_log_model(predictor, "model")
 })
